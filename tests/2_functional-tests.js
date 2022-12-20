cf4cec14539728bb6aa3dd26c6ccc38c56213f0a
Paul C <pecardozo17@gmail.com>
2022-12-19 23:29:28 -0300

    Solve functional tests assertion #4

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -36,28 +36,47 @@ suite('Functional Tests', function () {
       chai
         .request(server)
         .put('/travellers')
-
+        .send({
+          "surname":"Colombo"
+        })
         .end(function (err, res) {
-          assert.fail();
-
+          assert.equal(res.status,200);
+          assert.equal(res.type,'application/json');
+          assert.equal(res.body.name,'Cristoforo');
+          assert.equal(res.body.surname,'Colombo');
           done();
         });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
-      done();
+      chai.request(server)
+      .put('/travellers')
+      .send({
+        "surname":"da Verrazzano"
+      })
+      .end(function(err,res){
+        assert.equal(res.status,200);
+        assert.equal(res.type,'application/json');
+        assert.equal(res.body.name,'Giovanni');
+        assert.equal(res.body.surname,'da Verrazzano');
+        done();
+      })
     });
   });
 });
 
 const Browser = require('zombie');
+Browser.site = 'localhost:3000';
+
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
+  const browser = new Browser();
 
-
+  suiteSetup(function(done) {
+    return browser.visit('/', done);
+    });
+  
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {
